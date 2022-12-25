4da84de0250147dc472aed1a70bfb8dd601d206d
va1e7ia <120427244+va1e7ia@users.noreply.github.com>
2022-12-25 16:21:53 +0300

    Добавлен вывод общей информации о задании

--- main-project/main-project/main-project.cpp
+++ main-project/main-project/main-project.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 
 int main()
-{
+{   
+    setlocale(LC_ALL,"ru");
+    std::cout << "Вариант 7. Банковские операции. Валерия Михалевич\n";
     std::cout << "Hello\n";
     return 0;
 }
