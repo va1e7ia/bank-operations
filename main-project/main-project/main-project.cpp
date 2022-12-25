3a4dcfcb8f4de1b3374e4394be30466c7ecd7433
va1e7ia <valeerriam@gmail.com>
2022-12-25 17:18:00 +0300

    Добавлены текстовые данные

--- main-project/main-project/main-project.cpp
+++ main-project/main-project/main-project.cpp
@@ -4,6 +4,6 @@ int main()
 {   
     setlocale(LC_ALL,"ru");
     std::cout << "Вариант 7. Банковские операции. Валерия Михалевич\n";
-    std::cout << "Hello\n";
+    std::cout << "Группа 12\n";
     return 0;
 }
